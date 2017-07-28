64623e3dfdc5a37500d1fb44d048321a78e6bfa6
ChiselD <chisel.delahanty@gmail.com>
2017-07-27 22:55:56 -0400

    use geolocation to display visitor coords on page

--- main.js
+++ main.js
@@ -23,18 +23,46 @@
 // 	$("#info").html(JSON.stringify(json));
 // });
 
-$(document).ready(function() {
+function success(position) {
+	let myLat = Math.round(position.coords.latitude);
+	let myLong = Math.round(position.coords.longitude);
+	$("#lat").html(myLat);
+	$("#long").html(myLong);
 
-  var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
-  var data = {
-    q : "Portland,OR",
-    units : "metric"
-  };
-  function showWeather(weatherReport) {
-    $('#temperature').text(weatherReport.main.temp);
-  }
-  $.getJSON(weatherAPI, data, showWeather);
+	const weatherAPI = "https://fcc-weather-api.glitch.me/api/current?lat=" + myLat + "&lon=" + myLong + "/";
+
+	console.log("myLat is: " + myLat);
+	console.log("myLong is: " + myLong);
+
+	console.log(weatherAPI);
+	const data = {
+		"weather": weatherAPI.weather[0].main,
+		"description": weatherAPI.weather[0].description,
+		"icon": weatherAPI.weather[0].icon,
+		"temp": weatherAPI.main.temp
+	};
+	function showWeather() {
+		$("#info").text(data.temp);
+	}
+  	$.getJSON(weatherAPI, data, showWeather);
+}
+
+function failure(position) {
+	$("#info").html("<p><p>Attempt to get coordinates failed :(</p>");
+}
+
+$(document).ready(function() {
+	let x = navigator.geolocation;
+	x.getCurrentPosition(success, failure);
 });
+
+//extra note to self:
+  	// $.getJSON(weatherAPI, function(json){
+  	// 	let yourTemp = json.main.temp;
+   //  	// $(".info").html(yourTemp);
+   //  	console.log(yourTemp);
+  	// });
+
 
 // // This JSON object is here merely for my visual reference!
 // { "coord": {
