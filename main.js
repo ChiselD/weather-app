ad513e297c2211ccad5092bde97e04550ea6be05
Jinx <chisel.delahanty@gmail.com>
2017-08-13 17:02:31 -0400

    add full JSON object commented out as ref

--- main.js
+++ main.js
@@ -58,3 +58,48 @@ function failure(position) {
 
 
 //////
+
+// This JSON object is here merely for my own reference!
+
+// {
+// 	"coord": {
+// 		"lon":-84,
+// 		"lat":34
+// 	},
+// 	"weather":[{
+// 		"id":802,
+// 		"main":"Clouds",
+// 		"description":"scattered clouds",
+// 		"icon":"https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F03d.png?1499366020890"
+// 	}],
+// 	"base":"stations",
+// 	"main":{
+// 		"temp":31.3,
+// 		"pressure":1015,
+// 		"humidity":58,
+// 		"temp_min":31,
+// 		"temp_max":32
+// 	},
+// 	"visibility":16093,
+// 	"wind":{
+// 		"speed":2.6,
+// 		"deg":300
+// 	},
+// 	"clouds":{
+// 		"all":40
+// 	},
+// 	"dt":1502654280,
+// 	"sys":{
+// 		"type":1,
+// 		"id":780,
+// 		"message":0.0043,
+// 		"country":"US",
+// 		"sunrise":1502621817,
+// 		"sunset":1502670226
+// 	},
+// 	"id":4205196,
+// 	"name":"Lawrenceville",
+// 	"cod":200
+// }
+
+//////
